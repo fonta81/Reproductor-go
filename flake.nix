d228bb8186b7c7827c27bc6632f1a7fcb06f799f
mteo <cortinamateo211@gmail.com>
2026-09-23 09:54:24 -0500

    feat(Nix): add Nix flake development enviroment

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Devshell para proyectos Go (bubbletea + oto)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Define los sistemas compatibles (añade más si usas macOS u otras arquitecturas)
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      
+      # Genera la configuración para cada sistema
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.go
+              pkgs.gopls
+              pkgs.pkg-config
+              pkgs.alsa-lib
+            ];
+
+            shellHook = ''
+              echo "Entorno Go listo (bubbletea + oto)"
+              go version
+            '';
+          };
+        }
+      );
+    };
+}
